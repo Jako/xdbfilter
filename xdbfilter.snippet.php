158b2e967c11a8dcca2ea728ad9825a6c5d37196
Thomas Jakobi <thomas.jakobi@partout.info>
2011-09-13 14:20:30 +0300

    Edited xdbfilter.snippet.php via GitHub

--- xdbfilter.snippet.php
+++ xdbfilter.snippet.php
@@ -3,7 +3,6 @@
  * xdbfilter
  * snippet to filter records from database
  * License GPL
- * Based upon: http://www.partout.info/css_modx.html
  * Version 0.3.1 (13.09.2011)
  * Author: Bruno Perner <b.perner@gmx.de>
  * 
